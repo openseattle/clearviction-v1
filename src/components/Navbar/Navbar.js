2cd1e41e39b5dfdeccf052905b73568a7900aa12
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2021-01-14 20:06:59 -0800

    Adds title to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React from 'react'
 
-import "./styles.scss";
-import CustomLink from "../CustomLink";
+import './styles.scss'
+import CustomLink from '../CustomLink'
 
 export const NavbarTemplate = ({ data }) => (
-  <nav className="navbar">
-    <div className="container  navbar-container">
+  <nav className='navbar'>
+    <div className='container  navbar-container'>
+      <span className='navbar-title'>
+        Washington Conviction Vacation Project
+      </span>
       {data.menuItems.length > 0 && (
-        <ul className="navbar-menu">
-          {data.menuItems.map(menuItem => (
-            <li key={menuItem.linkURL} className="navbar-menuItem">
+        <ul className='navbar-menu'>
+          {data.menuItems.map((menuItem) => (
+            <li key={menuItem.linkURL} className='navbar-menuItem'>
               <CustomLink
                 linkType={menuItem.linkType}
                 linkURL={menuItem.linkURL}
-                className="navbar-menuLink"
+                className='navbar-menuLink'
               >
                 {menuItem.label}
               </CustomLink>
@@ -23,14 +26,14 @@ export const NavbarTemplate = ({ data }) => (
       )}
     </div>
   </nav>
-);
+)
 
-const Navbar = props => {
+const Navbar = (props) => {
   if (!props.data) {
-    return null;
+    return null
   }
-  const data = props.data.edges[0].node.frontmatter;
-  return <NavbarTemplate data={data} />;
-};
+  const data = props.data.edges[0].node.frontmatter
+  return <NavbarTemplate data={data} />
+}
 
-export { Navbar };
+export { Navbar }
