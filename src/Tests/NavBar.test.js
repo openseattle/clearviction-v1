9c3d25b47676f14f022dc979abde76bc24cf565d
Lara Samuelson <lara.samuelson@gmail.com>
2021-06-03 09:18:15 -0700

    Snapshot test added, new utilities Enzyme, Enzyme configured

--- src/Tests/NavBar.test.js
+++ src/Tests/NavBar.test.js
@@ -1,14 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import NavBar from '../Components/NavBar'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from "enzyme";
+import toJson from "enzyme-to-json";
+import { BrowserRouter } from 'react-router-dom';
+import NavBar from '../Components/NavBar';
 
 describe("NavBar component", () => {
+    //smoke test: Why - Build Verification
     it('renders without crashing', () => {
         const div = document.createElement('div');
         ReactDOM.render(<BrowserRouter> <NavBar/> </BrowserRouter>, div);
         ReactDOM.unmountComponentAtNode(div);
     });
-
-
+    /*snapshot test: Why - You can see if our component is rendering correctly
+    Any changes to to the Dom of NavBar will result in a failed snapshot test.
+    We just need to press "w" to activate watch mode then press "u" to update the snapshot.
+    our snap shot file will be automatically updated with the new snapshot and will pass our tests*/
+    it('renders NavBar by default', () => {
+        const wrapper = shallow(<NavBar />);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
 })
