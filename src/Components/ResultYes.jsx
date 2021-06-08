8b03ed07ffa6caf6f33eab6784a962ac32c3f6ee
Janu <janu.m.sung@gmail.com>
2021-06-07 18:44:55 -0700

    added ResultYes Comp

---
+++ src/Components/ResultYes.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const ResultYes = () => {
+  return (
+    <div>
+      <h2>
+        From your answers, it seems that you may be eligible to vacate your
+        marijuana misdemeanor conviction!
+      </h2>
+      <p>
+        Your next step is to fill out a Motion and Declaration for Order
+        Vacating Marijuana Conviction.
+        <a href="https://www.courts.wa.gov/forms/?fa=forms.contribute&formID=38">
+          Court Form No. CrRLJ 09.0800
+        </a>
+      </p>
+
+      <p>
+        Steps to vacating a conviction in Washington state:
+        <a href="https://www.expeal.com/steps/#washington">Learn More</a>
+      </p>
+    </div>
+  );
+};
+
+export default ResultYes;
