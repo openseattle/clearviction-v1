9bdcff18c89f344603a7746855082d55e95d96d2
javila35 <josepharthuravila@gmail.com>
2021-06-09 09:02:45 -0700

    Fix footer style issue Footer is visible change the background color to match the button Fix calculator height Remove unused import in Header

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../CSS/layout.css";
 
 const Footer = () => {
   return (
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import NavBar from "./NavBar";
+
 const Header = () => {
   return (
     <div className="header">
