4907f23a4826d1587add6782274be6a72c3fbf2c
javila35 <josepharthuravila@gmail.com>
2021-06-09 15:56:37 -0700

    making some code ugly to test

--- src/Components/AboutDetails.jsx
+++ src/Components/AboutDetails.jsx
@@ -5,9 +5,31 @@ const AboutDetails = (props) => {
   return (
     <div class="container-sm">
       <img src={props.img} class="about-img" />
+
+
+
+
+
+      
       <div class="container" id="about-info">
+
+
+
+
+
+
+
         <h5>{props.name}</h5>
         <h6 id="about-title">{props.title}</h6>
+
+
+
+
+
+
+
+
+
         <p>{props.description}</p>
       </div>
     </div>
