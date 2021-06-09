63220aac3952a90fc303ce1a0bfa5425caef4d96
javila35 <josepharthuravila@gmail.com>
2021-06-09 10:04:46 -0700

    Remove unused routes Remove results_no link Set defualt userResponse to false: see comments

--- src/Pages/CalculatorQuestionContainer.jsx
+++ src/Pages/CalculatorQuestionContainer.jsx
@@ -4,32 +4,32 @@ import CalculatorQuestion from "../Components/CalculatorQuestion";
 import CalculatorResults from "../Components/CalculatorResults";
 import "../CSS/Calculator.css";
 
+/** Directs traffic to result screen or current question based on URL */
 const CalculatorQuestionCont = () => {
   let { path } = useRouteMatch();
 
+  /**
+   * Default userResponse is false to handle some edge cases
+   * 1. They click the link on the landing page because their conviction is not a MJ misdemeanor
+   * 2. They navigate to 'calculator/results' using their url bar
+   */
   const [userResponse, setUserResponse] = useState({
-    1: true,
-    2: true,
-    3: true,
-    4: true,
-    5: true,
-    6: true,
+    1: false,
+    2: false,
+    3: false,
+    4: false,
+    5: false,
+    6: false,
   });
 
+  
+
+  /** Return a switch that routes to result screen, or question to display */
   return (
     <>
       <Switch>
-        <Route path={`${path}/results_no`}>
-          <CalculatorResults
-            userResponse={userResponse}
-            setUserResponse={setUserResponse}
-          />
-        </Route>
         <Route path={`${path}/results`}>
-          <CalculatorResults
-            userResponse={userResponse}
-            setUserResponse={setUserResponse}
-          />
+          <CalculatorResults userResponse={userResponse} />
         </Route>
         <Route exact path={`${path}/:number`}>
           <CalculatorQuestion
