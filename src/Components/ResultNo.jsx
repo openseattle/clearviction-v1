f7f4bf89f629e8a986c716c5b0b1ab8ee55c3851
Katie <25576005+stuttskl@users.noreply.github.com>
2021-07-08 18:56:27 -0700

    modified copy of result no screen and removed list of reasons why someone may be ineligible

--- src/Components/ResultNo.jsx
+++ src/Components/ResultNo.jsx
@@ -1,34 +1,13 @@
 import React from "react";
-import Button from "./Button";
 
-const ResultNo = ({ noneligible }) => {
-  // TODO: Get red 'x' SVG to match figma file
-  const renderItems = () => {
-    return noneligible.map((item) => {
-      /** Skip blank entry */
-      if (!item) return;
-      return <li>{item}</li>;
-    });
-  };
-
-  const handleClick = () => {
-    // redirect to contact page
-    console.log("i was clicked to redirect to contact page");
-  };
-
+const ResultNo = () => {
   return (
     <>
       <p className="calc-col title">
-        As of now, you may NOT be eligible for vacation for these reasons:{" "}
+        As of now, it seems you may NOT be eligible to vacate your marijuana
+        misdemeanor conviction because you have not completed the terms of your
+        offense.
       </p>
-
-      <ul>{noneligible && renderItems()}</ul>
-
-      <p className="calc-col results">
-        Help us improve our eligibility calculator by telling us your story:
-      </p>
-
-      <Button className="calc-button" text="Contact Us" onClick={handleClick} />
     </>
   );
 };
