295d40e2cd5914fe5dc9ab5011d9d98adef99aeb
Lara Samuelson <lara.samuelson@gmail.com>
2021-07-29 13:21:45 -0700

    run prettier

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    // used to allow calculator to open a new tab in gitpages
-    BASE_URL: window.BASE_URL || "",
-}
+  // used to allow calculator to open a new tab in gitpages
+  BASE_URL: window.BASE_URL || "",
+};
