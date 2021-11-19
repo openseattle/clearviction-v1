3efba36f71063998481d65edb19cfbf6d55b07bc
sjpaige <sebastianpaige@gmail.com>
2021-11-19 13:43:08 -0800

    Moved to components folder

---
+++ src/Components/Tests/Footer.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import Footer from '../Footer'
+
+describe("Footer", () => {
+
+    const footer = shallow(<Footer />);
+
+    it("Should be a footer", () => {
+        
+        expect(footer.find(".footer-cont")).toHaveLength(1);
+    })
+
+    it("Should have links",  () => {
+        const link = () => <a className="test-link" href="https://www.test.org/"><ul>test</ul></a>
+        expect(footer.containsMatchingElement(link)).toEqual(true);
+    })
+});
