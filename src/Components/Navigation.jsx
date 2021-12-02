d698fa361b862b3a3ce9fd81f10d2b4f0dc2a43f
Katie <25576005+stuttskl@users.noreply.github.com>
2021-12-02 14:59:50 -0800

    added disclaimer to mobile nav

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,31 +1,47 @@
 import "../CSS/Navigation.css";
-import navLogo from '../Assets/cvp-logo-white.png';
+import navLogo from "../Assets/cvp-logo-white.png";
 
 const Navigation = () => {
-    return (
-        <>
-            <nav class="navbar navbar-expand-lg navbar-dark" id="nav">
-                <a class="navbar-brand" href="/"><img id="nav-logo" src={navLogo} alt="cvp logo"/>WA Conviction Vacation Project</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+  return (
+    <>
+      <nav class="navbar navbar-expand-lg navbar-dark" id="nav">
+        <a class="navbar-brand" href="/">
+          <img id="nav-logo" src={navLogo} alt="cvp logo" />
+          WA Conviction Vacation Project
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="/">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/calculator">Calculator</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/about">About</a>
-                    </li>
-                </ul>
-                </div>
-            </nav>
-        </>
-    );
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="/">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/calculator">
+                Calculator
+              </a>
+            </li>
+            <span id="navbar-text" class="navbar-text">
+              The information on this site is not, nor should it be, considered
+              legal advice.
+            </span>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Navigation;
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,25 +1,25 @@
-import FAQAccordion from '../Components/Accordion'
-import PrimaryButton from '../ui-kit/Button';
-import SecondaryButton from '../ui-kit/SecondaryButton';
-import Text from '../ui-kit/Text';
+import FAQAccordion from "../Components/Accordion";
+import PrimaryButton from "../ui-kit/Button";
+import SecondaryButton from "../ui-kit/SecondaryButton";
+import Text from "../ui-kit/Text";
 
-import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import Divider from '@mui/material/Divider';
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
 
-import cvpLogoWhite from '../Assets/cvp-logo-white.png'
+import cvpLogoWhite from "../Assets/cvp-logo-white.png";
 import cvpLogoLeftHalf from "../Assets/cvp-logo-white-left-half.png";
-import democracylabLogo from '../Assets/democracylab-logo.png'
-import openseattleLogo from '../Assets/openseattle-logo.png'
-import housing from '../Assets/housing.svg';
-import employment from '../Assets/employment.svg';
-import education from '../Assets/education.svg';
-import travel from '../Assets/travel.svg';
-import teamwork from '../Assets/teamwork.svg'
-import phone from '../Assets/phone.svg'
-import "../CSS/HomePage.css"; 
+import democracylabLogo from "../Assets/democracylab-logo.png";
+import openseattleLogo from "../Assets/openseattle-logo.png";
+import housing from "../Assets/housing.svg";
+import employment from "../Assets/employment.svg";
+import education from "../Assets/education.svg";
+import travel from "../Assets/travel.svg";
+import teamwork from "../Assets/teamwork.svg";
+import phone from "../Assets/phone.svg";
+import "../CSS/HomePage.css";
 
 const HomePage = () => {
   return (
@@ -27,21 +27,31 @@ const HomePage = () => {
       <div class="row light-bg">
         <div class="col-lg">
           <div id="hero-parent">
-          <div id="left-content">
-          <Text variant={"h2"} text={"Vacation Eligibility Calculator"}></Text>
-          <Text variant={"h3"} text={"Check your eligibility to vacate your conviction for free in less than 10 minutes!"}></Text>
-          <Text variant={"h5"} text={"Coming in January 2022!"}></Text>
+            <div id="left-content">
+              <Text
+                variant={"h2"}
+                text={"Vacation Eligibility Calculator"}
+              ></Text>
+              <Text
+                variant={"h3"}
+                text={
+                  "Check your eligibility to vacate your conviction for free in less than 10 minutes!"
+                }
+              ></Text>
+              <Text variant={"h5"} text={"Coming in January 2022!"}></Text>
+            </div>
+            <div id="right-content">
+              <img src={cvpLogoLeftHalf} id="hero-logo" alt={"CVP Logo"} />
+            </div>
           </div>
-          <div id="right-content"><img src={cvpLogoLeftHalf} id="hero-logo" alt={"CVP Logo"}/></div>
-        </div>
         </div>
       </div>
 
       <div class="row">
         <div class="col-lg">
-          <PrimaryButton text={"How It Works"} href={"#how-it-works"}/>
-          <PrimaryButton text={"FAQs"} href={"#faq"}/>
-          <PrimaryButton text={"Why Vacate"} href={"#why-vacate"}/>
+          <PrimaryButton text={"How It Works"} href={"#how-it-works"} />
+          <PrimaryButton text={"FAQs"} href={"#faq"} />
+          <PrimaryButton text={"Why Vacate"} href={"#why-vacate"} />
         </div>
       </div>
 
@@ -49,17 +59,32 @@ const HomePage = () => {
         <Text variant={"h3"} text={"How It Works"}></Text>
 
         <div class="col-sm">
-          <img className="icons" src={teamwork} alt={"Teamwork Icon"}/>
-          <br/><Text variant={"h5"} text={"We break down the laws into understandable language."} fontFamily={"Lora"}></Text>
+          <img className="icons" src={teamwork} alt={"Teamwork Icon"} />
+          <br />
+          <Text
+            variant={"h5"}
+            text={"We break down the laws into understandable language."}
+            fontFamily={"Lora"}
+          ></Text>
         </div>
 
         <div class="col-sm">
-          <img className="icons" src={phone} alt={"Mobile Phone Icon"}/>
-          <br/><Text variant={"h5"} text={"You answer a few simple yes/no questions."} fontFamily={"Lora"}></Text>
+          <img className="icons" src={phone} alt={"Mobile Phone Icon"} />
+          <br />
+          <Text
+            variant={"h5"}
+            text={"You answer a few simple yes/no questions."}
+            fontFamily={"Lora"}
+          ></Text>
         </div>
         <div class="col-sm">
-        <img className="icons" src={employment} alt={"Employment Icon"}/>
-        <br/><Text variant={"h5"} text={"This helps determine your vacation eligibility."} fontFamily={"Lora"}></Text>
+          <img className="icons" src={employment} alt={"Employment Icon"} />
+          <br />
+          <Text
+            variant={"h5"}
+            text={"This helps determine your vacation eligibility."}
+            fontFamily={"Lora"}
+          ></Text>
         </div>
         <Text variant={"h3"} text={"Coming in January 2022!"}></Text>
       </div>
@@ -67,79 +92,143 @@ const HomePage = () => {
       <div class="row light-bg" id="faq">
         <div class="col-sm">
           <Text variant="h3" text={"FAQs"}></Text>
-          <div id="accordion"><FAQAccordion /></div>
+          <div id="accordion">
+            <FAQAccordion />
+          </div>
         </div>
       </div>
 
       <div class="row" id="why-vacate">
         <Text variant={"h3"} text={"Why Vacate?"}></Text>
-        <Text variant={"h4"} text={"A conviction vacation seals the offense from your record, and will give you more chance to access:"}></Text>
+        <Text
+          variant={"h4"}
+          text={
+            "A conviction vacation seals the offense from your record, and will give you more chance to access:"
+          }
+        ></Text>
 
         <div class="col-sm">
-          <img className="icons" src={housing} alt={"Housing Icon"}/>
+          <img className="icons" src={housing} alt={"Housing Icon"} />
           <Text variant={"h6"} text={"Housing"} fontWeight={"600"}></Text>
-          <Text variant={"h5"} text={"Make it easier to find and be approved for rent or purchasing a home."} fontFamily={"Lora"}></Text>
+          <Text
+            variant={"h5"}
+            text={
+              "Make it easier to find and be approved for rent or purchasing a home."
+            }
+            fontFamily={"Lora"}
+          ></Text>
         </div>
 
         <div class="col-sm">
-          <img className="icons" src={employment} alt={"Employment Icon"}/>
+          <img className="icons" src={employment} alt={"Employment Icon"} />
           <Text variant={"h6"} text={"Employment"} fontWeight={"600"}></Text>
-          <Text variant={"h5"} text={"Reduce barriers to finding and obtaining employment."} fontFamily={"Lora"}></Text>
+          <Text
+            variant={"h5"}
+            text={"Reduce barriers to finding and obtaining employment."}
+            fontFamily={"Lora"}
+          ></Text>
         </div>
 
         <div class="col-sm">
-          <img className="icons" src={education} alt={"Education Icon"}/>
+          <img className="icons" src={education} alt={"Education Icon"} />
           <Text variant={"h6"} text={"Education"} fontWeight={"600"}></Text>
-          <Text variant={"h5"} text={"Apply for scholarships, programs, degrees or certificates."} fontFamily={"Lora"}></Text>
+          <Text
+            variant={"h5"}
+            text={"Apply for scholarships, programs, degrees or certificates."}
+            fontFamily={"Lora"}
+          ></Text>
         </div>
 
         <div class="col-sm">
-          <img className="icons" src={travel} alt={"Travel Icon"}/>
-          <Text variant={"h6"} text={"Government Assistance"} fontWeight={"600"}></Text>
-          <Text variant={"h5"} text={"Receive government help."} fontFamily={"Lora"}></Text>              
+          <img className="icons" src={travel} alt={"Travel Icon"} />
+          <Text
+            variant={"h6"}
+            text={"Government Assistance"}
+            fontWeight={"600"}
+          ></Text>
+          <Text
+            variant={"h5"}
+            text={"Receive government help."}
+            fontFamily={"Lora"}
+          ></Text>
         </div>
         <div class="row">
-        <div class="col-lg">
-          <SecondaryButton text={"Learn More"}/>
+          <div class="col-lg">
+            <SecondaryButton text={"Learn More"} />
+          </div>
         </div>
-      </div>        
       </div>
 
       <div class="row dark-bg">
         <div class="col-lg">
-          <br/><img src={cvpLogoWhite} id="cvp-logo" alt={"CVP Logo"}/>
-          <br/><Text variant={"h5"} fontFamily={"Lora"} text={"The Conviction Vacation Project connects volunteers passionate about reducing barriers and making it easier for those with convictions in Washington State."}></Text>
-          <br/>
-          <br/><Text variant={"h5"} fontFamily={"Lora"} text={"We’re working together to create a tool to make navigating the conviction vacation process easier. Currently we are designing and building an eligibility calculator to help people determine if their conviction is eligible for vacation."}></Text>
-          <br/><SecondaryButton text={"Join the Team"}/>
+          <br />
+          <img src={cvpLogoWhite} id="cvp-logo" alt={"CVP Logo"} />
+          <br />
+          <Text
+            variant={"h5"}
+            fontFamily={"Lora"}
+            text={
+              "The Conviction Vacation Project connects volunteers passionate about reducing barriers and making it easier for those with convictions in Washington State."
+            }
+          ></Text>
+          <br />
+          <br />
+          <Text
+            variant={"h5"}
+            fontFamily={"Lora"}
+            text={
+              "We’re working together to create a tool to make navigating the conviction vacation process easier. Currently we are designing and building an eligibility calculator to help people determine if their conviction is eligible for vacation."
+            }
+          ></Text>
+          <br />
+          <SecondaryButton text={"Join the Team"} />
         </div>
       </div>
 
       <div class="row">
         <div class="col-lg">
-          <Stack direction="row" sx={{ display: 'flex', padding: 5 }}>
-            <Grid container columns={2} spacing={6} justifyContent="space-between">
+          <Stack direction="row" sx={{ display: "flex", padding: 5 }}>
+            <Grid
+              container
+              columns={2}
+              spacing={6}
+              justifyContent="space-between"
+            >
               <Grid item>
                 <List>
-                  <ListItem><a href="/" style={{ color: '#000000' }}>Home</a></ListItem>
-                  <ListItem><a href="/" style={{ color: '#000000' }}>Volunteer</a></ListItem>
-                  <ListItem><a href="/calculator" style={{ color: '#000000' }}>Calculator</a></ListItem>
-                  <ListItem><a href="/" style={{ color: '#000000' }}>Resources</a></ListItem>
-                  <ListItem><a href="/about" style={{ color: '#000000' }}>About</a></ListItem>
-                  <ListItem><a href="/" style={{ color: '#000000' }}>Contact</a></ListItem>
+                  <ListItem>
+                    <a href="/" style={{ color: "#000000" }}>
+                      Home
+                    </a>
+                  </ListItem>
+                  <ListItem>
+                    <a href="/calculator" style={{ color: "#000000" }}>
+                      Calculator
+                    </a>
+                  </ListItem>
+                  <ListItem>
+                    <a href="/" style={{ color: "#000000" }}>
+                      Contact
+                    </a>
+                  </ListItem>
                 </List>
               </Grid>
-            
-              <Grid item sx={{ float: 'right', position: 'relative' }}>
-                <Stack direction="column" >
-                  <img src={openseattleLogo} alt={"Open Seattle Logo"}/>
-                  <img src={democracylabLogo} alt={"Democracy Lab Logo"}/>
+
+              <Grid item sx={{ float: "right", position: "relative" }}>
+                <Stack direction="column">
+                  <img src={openseattleLogo} alt={"Open Seattle Logo"} />
+                  <img src={democracylabLogo} alt={"Democracy Lab Logo"} />
                 </Stack>
               </Grid>
             </Grid>
           </Stack>
           <Divider sx={{ marginTop: 1, marginBottom: 1 }} />
-          <Text variant={"body"} text={"The information on this site is not, nor should it be, considered legal advice."}></Text>
+          <Text
+            variant={"body"}
+            text={
+              "The information on this site is not, nor should it be, considered legal advice."
+            }
+          ></Text>
         </div>
       </div>
     </div>
