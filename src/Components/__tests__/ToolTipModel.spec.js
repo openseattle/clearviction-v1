db5d754e4600e0f8e52816f61dd86de8b7a9ecee
sjpaige <sebastianpaige@gmail.com>
2021-12-03 15:08:29 -0800

    created test of props and text display

--- src/Components/__tests__/ToolTipModel.spec.js
+++ src/Components/__tests__/ToolTipModel.spec.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {mount, render, shallow} from 'enzyme';
+import {mount, shallow} from 'enzyme';
 import Modal from '@mui/material/Modal';
 import Button  from '@mui/material/Button';
 
 import ToolTipModel from '../ToolTipModal';
 
 describe("<ToolTipModal />", () => {
+
     it("renders a modal", () => {
         const wrapper = shallow(<ToolTipModel />);
         expect(wrapper.find(Modal)).toHaveLength(1);
@@ -15,5 +16,13 @@ describe("<ToolTipModal />", () => {
         const wrapper = shallow(<ToolTipModel />);
         expect(wrapper.find(Button)).toHaveLength(1);
     });
+
+    it("displays passed in props tooltip data on the button", () => {
+        const wrapper = mount(<ToolTipModel text={"test"}/>);
+        expect(wrapper.props().text).toEqual("test");
+
+        const button = wrapper.find(Button);
+        expect(button.text()).toEqual("test");
+    })
 
 });
