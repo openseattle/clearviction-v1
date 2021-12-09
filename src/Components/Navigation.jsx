8b64488bd553a3f1251cbadb9571cad154dac32e
sjpaige <sebastianpaige@gmail.com>
2021-12-09 10:55:33 -0800

    changed to className

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -4,36 +4,36 @@ import navLogo from "../Assets/cvp-logo-white.png";
 const Navigation = () => {
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-dark" id="nav">
-        <a class="navbar-brand" href="/">
-          <img id="nav-logo" src={navLogo} alt="cvp logo" />
-          WA Conviction Vacation Project
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="/">
+      <nav className="navbar navbar-expand-lg navbar-dark" id="nav">
+          <a className="navbar-brand" href="/">
+            <img id="nav-logo" src={navLogo} alt="cvp logo" />
+            WA Conviction Vacation Project
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+   
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="/">
                 Home
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/calculator">
+            <li className="nav-item">
+              <a className="nav-link" href="/calculator">
                 Calculator
               </a>
             </li>
-            <span id="navbar-text" class="navbar-text">
+            <span id="navbar-text" className="navbar-text">
               The information on this site is not, nor should it be, considered
               legal advice.
             </span>
