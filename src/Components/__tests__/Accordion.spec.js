ce1995948ed5814944a341ca0eb5f96148fbff83
sjpaige <sebastianpaige@gmail.com>
2021-12-09 17:03:10 -0800

    completing test todos

--- src/Components/__tests__/Accordion.spec.js
+++ src/Components/__tests__/Accordion.spec.js
@@ -1,22 +1,39 @@
 import react from 'react';
 import {shallow} from 'enzyme';
+import { Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
 
+import Text from '../../ui-kit/Text';
 import FAQAccordian from '../Accordion';
 
-describe("<FAQAccordian", () => {
+describe("<FAQAccordian />", () => {
     let wrapper;
     beforeEach( () => {
         wrapper = shallow(<FAQAccordian />)
     });
 
     describe("ui", () => {
-        it.todo("renders the summary");
-        it.todo("renders the details");
-        it.todo("renders the text");
+
+        it("renders accordian elements", () => {
+            expect(wrapper.find(Accordion)).not.toHaveLength(0);
+        });
+        it("renders the summarys", () => {
+            expect(wrapper.find(AccordionSummary)).not.toHaveLength(0);
+        });
+        it("renders the details", () => {
+            expect(wrapper.find(AccordionDetails)).not.toHaveLength(0);
+        });
+        it("renders the text", () => {
+            expect(wrapper.find(Text)).not.toHaveLength(0);
+        });
     });
 
     describe("clicking the accordion", () => {
-        it.todo("opens the accordian");
+        it("opens the accordian", () => {
+            const accordion = wrapper.find(Accordion).at(0);
+            accordion.simulate("click");
+            console.log(accordion.props().debug())
+            expect(accordion.props().expanded).toEqual(true)
+        });
         it.todo("closes the accordian");
     });
 })
