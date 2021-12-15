b2a8aec5f2cb8b09f5cbfaf65080ab2654862e7f
Katie <25576005+stuttskl@users.noreply.github.com>
2021-12-14 19:42:55 -0800

    formatting, spacing fixes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,28 @@
 import "./App.css";
-
-import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
-import AboutPage from "./Pages/AboutPage";
 import HomePage from "./Pages/HomePage";
 import Navigation from "./Components/Navigation";
 import CalculatorPage from "./Pages/CalculatorPage";
 import ErrorPage from "./Pages/ErrorPage";
-import { CalcProvider } from "./calcContext";
 
 function App() {
   return (
     <div>
-      <Navigation />
-      <Switch>
+        <Navigation />
+        <Switch>
         <Route path="/404">
-          <ErrorPage />
-        </Route>
-        <Route path="/about">
-          <AboutPage />
+            <ErrorPage />
         </Route>
         <Route exact path="/">
-          <HomePage />
+            <HomePage />
         </Route>
-        <CalcProvider>
-          <Route path="/calculator/:pageId">
+        <Route path="/calculator/:pageId">
             <CalculatorPage />
-          </Route>
-          <Route exact path="/calculator">
+        </Route>
+        <Route exact path="/calculator">
             <Redirect to="/calculator/landing-0" />
-          </Route>
-        </CalcProvider>
-      </Switch>
+        </Route>
+        </Switch>
     </div>
   );
 }
--- src/Pages/CalculatorPage.jsx
+++ src/Pages/CalculatorPage.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import calculatorPages from "../data/calculatorPages";
+import calculatorPages from "../Data/calculatorPages";
 import ToolTipModal from "../Components/ToolTipModal";
 import Text from "../ui-kit/Text";
 
 /** MATERIAL UI IMPORTS */
-import Box from '@material-ui/core/Box';
-import Link from '@mui/material/Link';
+import Box from "@material-ui/core/Box";
+import Link from "@mui/material/Link";
 import Container from "@mui/material/Container";
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 
 const BUTTON_COLORS = {
   blue: "#4e6c99",
@@ -29,26 +29,22 @@ const CalculatorPage = () => {
   const renderButtons = (buttons) =>
     buttons.map(({ text, href, color }) => {
       return (
-        <Stack key={text} direction="column" style={{padding: 10}}>
+        <Stack key={text} direction="column" style={{ padding: 10 }}>
           <Button
             variant="contained"
             href={href}
-            style={
-              { backgroundColor: BUTTON_COLORS[color || "blue"] }}
+            style={{ backgroundColor: BUTTON_COLORS[color || "blue"] }}
           >
             <Text text={text} variant={"h6"}></Text>
           </Button>
         </Stack>
       );
-    }
-  );
+    });
 
   const renderBody = ({ type, text, href, items }) => {
     switch (type) {
       case "paragraph":
-        return (
-          <Text key={text} text={text} variant={"h4"} ></Text>
-        );
+        return <Text key={text} text={text} variant={"h4"}></Text>;
       case "link":
         return (
           <Link href={href} target="_blank" rel="noreferrer" key={text}>
@@ -58,48 +54,34 @@ const CalculatorPage = () => {
       case "list":
         return (
           <ul key={items[0]}>
-            { items.map((item) => <li key={item}>{item}</li>) }
+            {items.map((item) => (
+              <li key={item}>{item}</li>
+            ))}
           </ul>
-        )
+        );
       default:
         break;
     }
   };
 
   return (
-    <Box 
+    <Box
       sx={{
         padding: 20,
       }}
     >
       <Container maxWidth="xs" padding={10}>
         <Stack direction="column" spacing={2}>
-          { header && 
-              <Text text={header} variant={"h3"}></Text>
-          }
-          { body && 
-            <Container maxWidth="sm">{ body.map(renderBody) }</Container>
-          }
-          { buttons && 
-            renderButtons(buttons)
-          }
-          { tooltip &&
-            <ToolTipModal
-              text={tooltip}
-            />     
-          }
-          { footerLink && (
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={footerLink.href}
-            >
+          {header && <Text text={header} variant={"h3"}></Text>}
+          {body && <Container maxWidth="sm">{body.map(renderBody)}</Container>}
+          {buttons && renderButtons(buttons)}
+          {tooltip && <ToolTipModal text={tooltip} />}
+          {footerLink && (
+            <a target="_blank" rel="noreferrer" href={footerLink.href}>
               {footerLink.text}
             </a>
-          ) }
-          { disclaimer && 
-            <Text text={disclaimer} variant={"h6"}></Text>
-          }
+          )}
+          {disclaimer && <Text text={disclaimer} variant={"h6"}></Text>}
         </Stack>
       </Container>
     </Box>
