b260d1a02b7035b7e2820da0424027e378e53d3a
laradei <lara.samuelson@gmail.com>
2021-12-18 09:48:31 -0800

    ran prettier

--- src/data/calculatorPagesTypes.ts
+++ src/data/calculatorPagesTypes.ts
@@ -65,25 +65,25 @@ interface FooterLink {
 }
 
 export const BUTTON_TEXT = {
-    YES: "Yes",
-    NO: "No",
-    NA: "Not Applicable",
-    CONTINUE: "Continue"
-}
+  YES: "Yes",
+  NO: "No",
+  NA: "Not Applicable",
+  CONTINUE: "Continue",
+};
 
 export const BRANCH_TYPES = {
-    MAIN: "main",
-    ELIGIBLE: "elg",
-    INELIGIBLE: "inelg",
-    PROSTITUTION: "pro",
-    FISHING: "fsh",
-    MARIJUANA: "mar"
-}
+  MAIN: "main",
+  ELIGIBLE: "elg",
+  INELIGIBLE: "inelg",
+  PROSTITUTION: "pro",
+  FISHING: "fsh",
+  MARIJUANA: "mar",
+};
 
 export const QUESTION_NO = {
-    ONE: "1",
-    TWO: "2",
-    THREE: "3", 
-    FOUR: "4",
-    FIVE: "5"
-}
+  ONE: "1",
+  TWO: "2",
+  THREE: "3",
+  FOUR: "4",
+  FIVE: "5",
+};
