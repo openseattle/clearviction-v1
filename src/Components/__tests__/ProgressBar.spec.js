7e489f3cd7929e562466050af0c8819f062afc84
Lara Samuelson <lara.samuelson@gmail.com>
2021-12-22 18:59:07 -0800

    added active and current bar render tests

--- src/Components/__tests__/ProgressBar.spec.js
+++ src/Components/__tests__/ProgressBar.spec.js
@@ -5,20 +5,30 @@ import ProgressBar from "../ProgressBar";
 describe("<ProgressBar />", () => {
   let progressBar;
   const props = {
-    currentSectionName: "conv",
-    totalSections: 7,
+    currentSectionName: "circ",
+    totalSections: 3,
   };
   beforeEach(() => {
     progressBar = shallow(<ProgressBar {...props} />);
   });
 
-  it("should be a progressbar", () => {
-    expect(progressBar.find(".progressBar-Wrapper")).toHaveLength(1);
+  it("should render a progressbar", () => {
+    expect(progressBar.exists(".progressBar-Wrapper")).toEqual(true);
   });
   it("renders the li", () => {
     expect(progressBar.exists("li")).toEqual(true);
   });
   it("renders the title", () => {
     expect(progressBar.exists(".progress-bar-title")).toEqual(true);
+  });
+  it("renders the current bar correctly", () => {
+    expect(
+      progressBar.find(".progressBar-current").prop("style")
+    ).toHaveProperty("width", "68%");
+  });
+  it("renders the active bar correctly", () => {
+    expect(
+      progressBar.find(".progressBar-active").prop("style")
+    ).toHaveProperty("width", "34%");
   });
 });
