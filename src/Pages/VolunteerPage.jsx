1629e7d6f11d7ba9f3916f14343cd5fb48772e22
sjpaige <sebastianpaige@gmail.com>
2022-01-06 17:47:32 -0800

    Second pass volunteer page mvp

--- src/Pages/VolunteerPage.jsx
+++ src/Pages/VolunteerPage.jsx
@@ -5,9 +5,13 @@ import {
   Typography,
   Card,
   CardContent,
+  Container,
+  CardMedia,
 } from "@material-ui/core";
 import placeholder from "../Assets/placeholder_contact.png";
 import HeroPanel from "../Components/HeroPanel";
+import InfoCard from "../Components/InfoCard";
+import RolesDisplay from "../Components/RolesDisplay";
 import SecondaryButton from "../ui-kit/SecondaryButton";
 
 const useStyles = makeStyles({
@@ -15,30 +19,35 @@ const useStyles = makeStyles({
     color: "white",
     marginBottom: 20,
   },
+  textAltColor: {
+    color: "black",
+    marginBottom: 20,
+  },
 });
 
-const roles = [
-  "User Research",
-  "Front-End Development",
-  "UX Design",
-  "Copywriting",
-  "Back-End Development",
-  "Marketing",
-  "Subject Matter Expert",
-  "Grant Writing",
-];
 const VolunteerPage = () => {
   const classes = useStyles();
   return (
     <>
       <HeroPanel title="Volunteer" />
-      <Box style={{ backgroundColor: "var(--dark-blue)" }}>
-        <Grid container spacing={5}>
-          <Grid item xs={12} sm={6} md={6} align="center">
-            <Box style={{ width: "100%" }} component="img" src={placeholder} />
-          </Grid>
-          <Grid item xs={12} sm={6} md={6}>
-            <Box>
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        style={{ backgroundColor: "var(--dark-blue)" }}
+      >
+        <Grid item md={6}>
+          <Box justifyContent="center" display="flex">
+            <Box
+              component="img"
+              src={placeholder}
+              style={{ maxHeight: "250px", maxWidth: "500px", margin: "2em" }}
+            />
+          </Box>
+        </Grid>
+        <Grid item md={6}>
+          <Box justifyContent="center" display="flex">
+            <Box style={{ maxWidth: "500px", margin: "2em" }}>
               <Typography className={classes.textColor} variant="h2">
                 Why Volunteer?
               </Typography>
@@ -53,59 +62,72 @@ const VolunteerPage = () => {
                 to make it easier for people to move forward.
               </Typography>
             </Box>
-          </Grid>
-        </Grid>
-      </Box>
-      <Grid container spacing={5}>
-        <Grid item md={6}>
-          <Box style={{ backgroundColor: "var(--light-blue)" }}>
-            <Typography className={classes.textColor} variant="h2">
-              Who can apply?
-            </Typography>
-            <ul style={{ listStyleType: "disc", color: "white" }}>
-              <li>You can commit to 4 hours a week for at least 6 months</li>
-              <li>
-                You want to share your expertise in a field to help us make a
-                better product
-              </li>
-              <li>
-                You are self-motivated and are comfortable with remote work
-              </li>
-            </ul>
-          </Box>
-        </Grid>
-        <Grid item md={6}>
-          <Box elevation={0}>
-            <Typography variant="h2">How can you apply?</Typography>
-            <Typography variant="body1">
-              We are currently using DemocracyLab to streamline the volunteering
-              process, which you can find clicking the button below.
-            </Typography>
-            <Typography variant="body1">
-              If you want further information and want to have a chat, don’t
-              hesitate to contact us though our form.
-            </Typography>
           </Box>
         </Grid>
       </Grid>
-
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        style={{ marginTop: "0em" }}
+      >
+        <Grid item md={6}>
+          <Box
+            justifyContent="center"
+            display="flex"
+            style={{ backgroundColor: "var(--light-blue)" }}
+          >
+            <Box style={{ maxWidth: "500px", margin: "2em" }}>
+              <Typography variant="h2" className={classes.textColor}>
+                Who can apply?
+              </Typography>
+              <Typography className={classes.textColor}>
+                <ul
+                  style={{
+                    listStyleType: "disc",
+                  }}
+                >
+                  <li>
+                    You can commit to 4 hours a week for at least 6 months
+                  </li>
+                  <li>
+                    You want to share your expertise in a field to help us make
+                    a better product
+                  </li>
+                  <li>
+                    You are self-motivated and are comfortable with remote work
+                  </li>
+                </ul>
+              </Typography>
+            </Box>
+          </Box>
+        </Grid>
+        <Grid item md={6}>
+          <Box
+            justifyContent="center"
+            display="flex"
+            style={{ backgroundColor: "white" }}
+          >
+            <Box style={{ maxWidth: "500px", margin: "2em" }}>
+              <Typography variant="h2">How can you apply?</Typography>
+              <Typography variant="body1">
+                We are currently using DemocracyLab to streamline the
+                volunteering process, which you can find clicking the button
+                below.
+              </Typography>
+              <Typography variant="body1">
+                If you want further information and want to have a chat, don’t
+                hesitate to contact us though our form.
+              </Typography>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
       <Box style={{ backgroundColor: "var(--dark-blue)" }}>
-        <Typography variant="h2" style={{ color: "white" }}>
+        <Typography className={classes.textColor} variant="h2">
           Open Roles
         </Typography>
-        <Grid container spacing={3} justifyContent="center">
-          {roles.map((role) => (
-            <Grid item key={role}>
-              <Card style={{ width: 100, height: 100 }}>
-                <CardContent>
-                  <Typography variant="body1" align="center">
-                    {role}
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
+        <RolesDisplay />
         <Box justifyContent="center" display="flex">
           <SecondaryButton text="Join Us" />
         </Box>
