67dcd3ab8ca9d6a2736b761aad50377be6b8f12d
sjpaige <sebastianpaige@gmail.com>
2022-01-06 19:24:58 -0800

    create theme in parallell to the theme branch

---
+++ src/Components/Theme.jsx
@@ -0,0 +1,39 @@
+import { createTheme } from "@material-ui/core";
+
+const theme = createTheme({
+  palette: {
+    // we can have seperate light and dark themes if the designers want to build one
+    type: "light",
+    primary: {
+      // MAIN = dark blue, LIGHT = light blue
+      main: "#2d3047",
+      light: "#4e6c99",
+    },
+    secondary: {
+      // MAIN = dark gray, LIGHT = light gray
+      main: "#c4c4c4",
+      light: "#dcdcdc",
+    },
+    success: {
+      main: "#419D77",
+    },
+  },
+
+  typography: {
+    body1: "Lora",
+  },
+  overrides: {
+    MuiButton: {
+      root: {
+        width: "327px",
+        margin: "10px",
+      },
+      text: {
+        padding: "10px",
+        fontSize: "16px",
+      },
+    },
+  },
+});
+
+export default theme;
