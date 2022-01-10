d22bc9fff810d7fc5c9bf8839e4176006939ba43
sjpaige <sebastianpaige@gmail.com>
2022-01-10 14:33:08 -0800

    center error message

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -8,17 +8,15 @@ const ErrorPage = () => {
   useEffect(() => trackPageview("Error"), []);
 
   return (
-    <Box
-      sx={{
-        padding: 20,
-      }}
-    >
+ 
       <Container maxWidth="xs" padding={10}>
-        <Grid container direction="column" spacing={2}>
-          <Typography variant="h5">Oops, page not found!</Typography>
+        <Grid container direction="column" justifyContent="center">
+          <Grid item>
+          <Typography variant="h5" align="center">Oops, page not found!</Typography>
+            
+          </Grid>
         </Grid>
       </Container>
-    </Box>
   );
 };
 
