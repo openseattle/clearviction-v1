ad97e9bf6e4cd1140058f26dc69a52ed863d4850
sjpaige <sebastianpaige@gmail.com>
2022-01-12 20:59:44 -0800

    Add second option for layout

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -18,30 +18,56 @@ import {
   PanToolOutlined,
 } from "@material-ui/icons";
 
+import lawyer from "../Assets/lawyer.svg";
+
 const team = [1, 2, 3, 4, 5, 6];
 
 const useStyles = makeStyles((theme) => ({
-  actionItem: {
-    backgroundColor: theme.palette.primary.main,
-    color: "white",
-  },
-  infoDark: {
-    backgroundColor: theme.palette.primary.main,
-    color: "white",
-    borderRadius: 0,
+  alignItemContent: {
+    display: "flex",
+    justifyContent: "center",
   },
   roots: {
     backgroundColor: theme.palette.primary.light,
     color: "white",
     borderRadius: 0,
-    padding: "3rem",
+    padding: "2rem",
+  },
+  rootsTitle: {
+    padding: theme.spacing(1),
+  },
+  rootsImage: {
+    width: 300,
+    height: 180,
+    // [theme.breakpoints.down("sm")]: {
+    //   width: 150,
+    //   height: 90,
+    // },
   },
   rootsBodyText: {
-    maxWidth: 750,
+    minWidth: 150,
   },
-  theProblemRoot: {
+  theProblemRootleft: {
+    backgroundColor: "white",
+    color: "black",
+    padding: "3rem",
+  },
+  problemLeftTitle: {
+    paddingBottom: "2rem",
+  },
+  problemLeftBody: {
+    paddingBottom: "2rem",
+  },
+  problemRootRight: {
     backgroundColor: theme.palette.primary.main,
     color: "white",
+    padding: "1rem",
+    [theme.breakpoints.up("md")]: {
+      padding: "6rem",
+    },
+  },
+  problemRightListItem: {
+    padding: "2rem",
   },
   theProblemIcon: {
     color: "#FFD200",
@@ -50,12 +76,21 @@ const useStyles = makeStyles((theme) => ({
   ourMission: {
     backgroundColor: theme.palette.primary.light,
     color: "white",
-    padding: "3rem",
+    paddingTop: "10rem",
+    paddingBottom: "10rem",
+  },
+  ourMissionText: {
+    margin: theme.spacing(2),
+    maxWidth: 750,
+    textAlign: "center",
+  },
+  ourJourneyTitle: {
+    margin: "3rem",
   },
   ourTeam: {
     backgroundColor: theme.palette.primary.main,
-    marginTop: "2rem",
-    marginBottom: "2rem",
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
   },
 }));
 
@@ -71,46 +106,96 @@ const AboutPage = () => {
         <HeroPanel title={pageTitle} subtitle={pageSubtitle} />
 
         <Box className={classes.roots}>
-          <Typography variant="h2">The roots of our project</Typography>
+          <Typography className={classes.rootsTitle} variant="h2">
+            The roots of our project
+          </Typography>
+
+          {/* First Solution */}
+          {/* <List>
+            <ListItem >
+              <ListItemIcon>
+                <Box
+                  component="img"
+                  src={lawyer}
+                  className={classes.rootsImage}
+                />
+              </ListItemIcon>
+              <Typography className={classes.rootsBodyText} variant="body1">
+                One in four Washingtonians have been involved in the criminal
+                justice system. Those with a criminal record face significant
+                barriers to daily life after completing their prison terms.
+              </Typography>
+            </ListItem>
+            <ListItem>
+              <Typography className={classes.rootsBodyText} variant="body1">
+                Washington State’s New Hope Act makes it easier for people with
+                past criminal records to have their convictions vacated. Yet the
+                system is slow and inefficient.
+              </Typography>
+              <ListItemIcon>
+                <Box
+                  component="img"
+                  src={lawyer}
+                  className={classes.rootsImage}
+                />
+              </ListItemIcon>
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <Box
+                  component="img"
+                  src={lawyer}
+                  className={classes.rootsImage}
+                />
+              </ListItemIcon>
+              <Typography className={classes.rootsBodyText} variant="body1">
+                We have identified opportunities to use technology to streamline
+                this process and want to use our resources to appropriately met
+                user needs, creating an easier way to vacate convictions.
+              </Typography>
+            </ListItem>
+          </List> */}
+
+          {/* Alternative Solution */}
           <Grid container justifyContent="center">
-            <Grid item md={6}>
+            <Grid className={classes.alignItemContent} item xs={12} sm={6}>
               <Box
                 component="img"
-                src="#"
-                style={{ height: 180, width: 300 }}
+                src={lawyer}
+                className={classes.rootsImage}
               />
             </Grid>
-            <Grid item md={6}>
+            <Grid className={classes.alignItemContent} item xs={12} sm={6}>
               <Typography className={classes.rootsBodyText} variant="body1">
                 One in four Washingtonians have been involved in the criminal
                 justice system. Those with a criminal record face significant
                 barriers to daily life after completing their prison terms.
               </Typography>
             </Grid>
-            <Grid item md={6}>
+            <Grid className={classes.alignItemContent} item xs={12} sm={6}>
               <Box
                 component="img"
-                src="#"
-                style={{ height: 180, width: 300 }}
+                src={lawyer}
+                className={classes.rootsImage}
               />
             </Grid>
-            <Grid item md={6}>
-              <Typography variant="body1">
+            <Grid item xs={12} sm={6}>
+              <Typography className={classes.rootsBodyText} variant="body1">
                 Washington State’s New Hope Act makes it easier for people with
                 past criminal records to have their convictions vacated. Yet the
                 system is slow and inefficient.
               </Typography>
             </Grid>
-            <Grid item md={6}>
+            <Grid className={classes.alignItemContent} item xs={12} sm={6}>
               <Box
                 component="img"
-                src="#"
-                style={{ height: 180, width: 300 }}
+                src={lawyer}
+                className={classes.rootsImage}
               />
             </Grid>
 
-            <Grid item md={6}>
-              <Typography variant="body1">
+            <Grid className={classes.alignItemContent} item xs={12} sm={6}>
+              <Typography className={classes.rootsBodyText} variant="body1">
                 We have identified opportunities to use technology to streamline
                 this process and want to use our resources to appropriately met
                 user needs, creating an easier way to vacate convictions.
@@ -118,28 +203,29 @@ const AboutPage = () => {
             </Grid>
           </Grid>
         </Box>
-        <Box>
-          <Grid container>
-            <Grid item xs={12} sm={6}>
-              <Typography variant="h2">the problem</Typography>
-              <Typography variant="body1">
+        <Box style={{padding: "6rem"}}>
+          <Grid container >
+            <Grid className={classes.theProblemRootleft} item xs={12} sm={6}>
+              <Typography className={classes.problemLeftTitle} variant="h2">
+                The problem
+              </Typography>
+              <Typography className={classes.problemLeftBody} variant="body1">
                 The process of vacating a conviction is convoluted, making it
                 difficult for most people to navigate.
               </Typography>
-              <Typography variant="body1">
+              <Typography className={classes.problemLeftBody} variant="body1">
                 It requires a number of steps that can be difficult to
                 accomplish:
-                <ol>
-                  <li>Gathering documents</li>
-                  <li>Determining eligibility</li>
-                  <li>
-                    Filing motions with the court Scheduling hearings with a
-                    judge
-                  </li>
-                </ol>
+              </Typography>
+              <Typography component="ol" variant="body1">
+                <li>Gathering documents</li>
+                <li>Determining eligibility</li>
+                <li>
+                  Filing motions with the court Scheduling hearings with a judge
+                </li>
               </Typography>
             </Grid>
-            <Grid className={classes.theProblemRoot} item xs={12} sm={6}>
+            <Grid className={classes.problemRootRight} item xs={12} sm={6}>
               <List>
                 <ListItem>
                   <ListItemIcon>
@@ -174,26 +260,36 @@ const AboutPage = () => {
           </Grid>
         </Box>
         <Box className={classes.ourMission}>
-          <Typography variant="h4" align="center">
+          <Typography className={classes.ourMissionText} variant="h4">
             Our mission is to benefit individuals with criminal convictions and
             decrease lifelong collateral consequences.
           </Typography>
-          <Typography variant="h4" align="center">
-            To support this mission, we first focused on creating a <span style={{color: "#FFD200"}}>Conviciton
-            Eligibility Calculator</span> to help people determine if they are eligible
-            to vacate their conviction.
+          <Typography className={classes.ourMissionText} variant="h4">
+            To support this mission, we first focused on creating a{" "}
+            <span style={{ color: "#FFD200" }}>
+              Conviciton Eligibility Calculator
+            </span>{" "}
+            to help people determine if they are eligible to vacate their
+            conviction.
           </Typography>
         </Box>
-        <Box>
-          <Typography variant="h2">the process</Typography>
+        <Box style={{ height: 750 }}>
+          <Typography className={classes.ourJourneyTitle} variant="h2">
+            Our journey
+          </Typography>
         </Box>
         <Box className={classes.ourTeam}>
-          <Typography style={{ margin: "3rem", color: "white" }} variant="h2">
+          <Typography style={{ padding: "32px", color: "white" }} variant="h2">
             The Team
           </Typography>
-          <Grid container spacing={4} justifyContent="center" alignItems="center">
+          <Grid
+            container
+            spacing={4}
+            justifyContent="center"
+            alignItems="center"
+          >
             {team.map((id) => (
-                <TeamCard teamNumber={id}/>
+              <TeamCard key={id} teamNumber={id} />
             ))}
           </Grid>
         </Box>
