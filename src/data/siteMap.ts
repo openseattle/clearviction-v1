fbf265a745b161b3c215734a35b2f0f930a8eb5b
sjpaige <sebastianpaige@gmail.com>
2022-01-28 23:30:54 -0800

    link order

--- src/data/siteMap.ts
+++ src/data/siteMap.ts
@@ -13,15 +13,6 @@ const pages: Page[] = [
     {name: "Government Assistance", url: "/why-vacate/government-assistance" }
   ]},
   {
-    name: "Get Involved",
-    url: "/get-involved",
-    subpages: [
-      { name: "Volunteer", url: "/get-involved/volunteer" },
-      { name: "Donate", url: "/get-involved/donate" },
-      { name: "Partner with Us", url: "/get-involved/partner-with-us" },
-    ],
-  },
-  {
     name: "Resources",
     url: "/resources",
     subpages: [
@@ -29,6 +20,15 @@ const pages: Page[] = [
       { name: "Housing Assistance", url: "/resources/assistance/housing"},
       { name: "Employment Assistance", url: "/resources/assistance/employment"},
       { name: "Education Assistance", url: "/resources/assistance/education"},
+    ],
+  },
+  {
+    name: "Get Involved",
+    url: "/get-involved",
+    subpages: [
+      { name: "Volunteer", url: "/get-involved/volunteer" },
+      { name: "Donate", url: "/get-involved/donate" },
+      { name: "Partner with Us", url: "/get-involved/partner-with-us" },
     ],
   },
   { name: "About", url: "/about" },
