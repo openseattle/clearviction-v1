0b0d5ac5a7d3f2ab99c1c69a76cfb674480b4da4
sjpaige <sebastianpaige@gmail.com>
2022-02-10 20:56:12 -0800

    add bullet points

--- src/Subpages/WhyVacate/EducationOccLicReq.jsx
+++ src/Subpages/WhyVacate/EducationOccLicReq.jsx
@@ -37,31 +37,31 @@ export const EducationOccLicReq = () => {
             <Grid item xs={12} sm={6} md={6}>
               <List>
                 <CVPListItem
-                  useBulletPoint={false}
+                  useBulletPoint={true}
                   textAlignment={"left"}
                   text={
                     "Amount of time that has elapsed since the last criminal activity"
                   }
                 />
                 <CVPListItem
-                  useBulletPoint={false}
+                  useBulletPoint={true}
                   textAlignment={"left"}
                   text={
                     "Work activity of the applicant before and after the criminal conviction"
                   }
                 />
                 <CVPListItem
-                  useBulletPoint={false}
+                  useBulletPoint={true}
                   textAlignment={"left"}
                   text={"Age of the person when the crime was committed"}
                 />
                 <CVPListItem
-                  useBulletPoint={false}
+                  useBulletPoint={true}
                   textAlignment={"left"}
                   text={"The nature and seriousness of the crime"}
                 />
                 <CVPListItem
-                  useBulletPoint={false}
+                  useBulletPoint={true}
                   textAlignment={"left"}
                   text={
                     "Extent to which a license might offer an opportunity to engage in further criminal activity"
@@ -72,35 +72,35 @@ export const EducationOccLicReq = () => {
             <Grid item xs={12} sm={6} md={6}>
               <List>
                 <CVPListItem
-                  useBulletPoint={false}
+                  useBulletPoint={true}
                   textAlignment={"left"}
                   text={
                     "Evidence of the person’s rehabilitation while incarcerated or after release"
                   }
                 />
                 <CVPListItem
-                  useBulletPoint={false}
+                  useBulletPoint={true}
                   textAlignment={"left"}
                   text={
                     "Relationship of a crime to the ability, capacity, or fitness to perform the duties"
                   }
                 />
                 <CVPListItem
-                  useBulletPoint={false}
+                  useBulletPoint={true}
                   textAlignment={"left"}
                   text={
                     "Extent and nature of the applicant’s past criminal activity"
                   }
                 />
                 <CVPListItem
-                  useBulletPoint={false}
+                  useBulletPoint={true}
                   textAlignment={"left"}
                   text={
                     "Relationship of the crime to the purposes for requiring a license"
                   }
                 />
                 <CVPListItem
-                  useBulletPoint={false}
+                  useBulletPoint={true}
                   textAlignment={"left"}
                   text={
                     "Other evidence of the person’s fitness from people with knowledge of the applicant"
