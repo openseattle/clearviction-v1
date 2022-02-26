7d873bee5a603ce7f55e7d757f3d3458d85ff206
sjpaige <sebastianpaige@gmail.com>
2022-02-25 20:39:35 -0800

    update back button with error and redirect

--- src/ui-kit/BackButton.jsx
+++ src/ui-kit/BackButton.jsx
@@ -1,5 +1,7 @@
-import { Button, makeStyles } from "@material-ui/core";
+import { Button, makeStyles, Snackbar } from "@material-ui/core";
 import { ArrowBackIos } from "@material-ui/icons";
+import { Alert } from "@material-ui/lab";
+import { useState } from "react";
 import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles({
@@ -11,15 +13,50 @@ const useStyles = makeStyles({
 export const BackButton = (props) => {
   const classes = useStyles();
   let history = useHistory();
+  const [open, setOpen] = useState(false);
+
+  const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  const handleClick = () => {
+    // added error to handle the failure condition as an alert where users are informed
+    // about what is happening with a popup before they are redirected.
+    try {
+      if (document.referrer.includes("calculator")) {
+        history.goBack();
+      } else throw Error("nonCalcReferrer: unable to verify referral by a calculator question. Restarting Calculator...");
+    } catch (error) {
+      console.log(error);
+      setOpen(true);
+      sleep(4000).then(() => history.push("/calculator/landing-0"));
+    }
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
+
   return (
     <>
       <Button
         className={classes.backButtonStyle}
-        onClick={() => history.goBack()}
+        onClick={handleClick}
         {...props}
       >
         <ArrowBackIos /> Previous Question{" "}
       </Button>
+      <Snackbar
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      >
+        <Alert onClose={handleClose} severity="error">
+          Unable to access previous questions. Restarting...
+        </Alert>
+      </Snackbar>
     </>
   );
 };
