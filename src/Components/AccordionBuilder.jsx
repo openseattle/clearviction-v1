c47eb136a99586c4d3c8e5d8d07bdf23344651df
sjpaige <sebastianpaige@gmail.com>
2022-03-05 19:58:59 -0800

    attach props to correct component

--- src/Components/AccordionBuilder.jsx
+++ src/Components/AccordionBuilder.jsx
@@ -27,10 +27,8 @@ const AccordionBuilder = ({ faqs }) => {
           className={classes.faqAccordionStyle}
           variant="outlined"
         >
-          <AccordionSummary expandIcon={<ExpandMore />}>
-            <Typography variant="subtitle1" {...a11yProps(idx)}>
-              {faq.summary}
-            </Typography>
+          <AccordionSummary expandIcon={<ExpandMore />} {...a11yProps}>
+            <Typography variant="subtitle1">{faq.summary}</Typography>
           </AccordionSummary>
           <AccordionDetails>
             <Box display="block">{faq.details}</Box>
