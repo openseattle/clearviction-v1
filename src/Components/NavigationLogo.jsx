2b9746b55745d06b7eda81e8d4e4727752aa232f
sjpaige <sebastianpaige@gmail.com>
2022-03-07 12:38:33 -0800

    move to buttonbase

--- src/Components/NavigationLogo.jsx
+++ src/Components/NavigationLogo.jsx
@@ -1,8 +1,12 @@
-import { IconButton } from "@material-ui/core";
+import { ButtonBase } from "@material-ui/core";
 import logo from "../Assets/clearviction_logo_small_light 2.svg";
 
 export const NavigationLogo = () => (
-  <IconButton href="/" aria-label="clearviction home page" disableRipple>
-    <img src={logo} alt="clearviction" />
-  </IconButton>
+  <ButtonBase
+    href="/"
+    aria-label="clearviction home page"
+    disableRipple
+  >
+    <img src={logo} alt="clearviction logo" style={{ height: 40 }} />
+  </ButtonBase>
 );
