d6f826c8933b50fe20d35cd7fbaa9977e0660f46
sjpaige <sebastianpaige@gmail.com>
2022-04-09 12:44:38 -0700

    adjust special case for links

--- src/Pages/Calculator/SpecialCaseTitle.jsx
+++ src/Pages/Calculator/SpecialCaseTitle.jsx
@@ -1,4 +1,5 @@
 import { Grid, Typography } from "@material-ui/core";
+import { BodyType } from "../../data/calculatorPagesTypes";
 import { BackButton } from "../../ui-kit/BackButton";
 import PrimaryButton from "../../ui-kit/PrimaryButton";
 import { SpecialCaseTitleStyles } from "./SpecialCaseTitleStyles";
@@ -14,13 +15,37 @@ const SpecialCaseTitle = (props) => {
         {props.header}
       </Typography>
       <Grid item className={classes.bodyGrid}>
-        { props.body && props.body.map((b, idx) => {
-          return (
-            <Typography key={idx} variant="body2" className={classes.body}>
-              {b.text}
-            </Typography>
-          );
-        })}
+        {props.body &&
+          props.body.map((b, idx) => {
+            switch (b.type) {
+              default:
+                return (
+                  <Typography
+                    key={idx}
+                    variant="body2"
+                    className={classes.body}
+                  >
+                    {b.text}
+                  </Typography>
+                );
+              case BodyType.LIST:
+                return (
+                  <ul>
+                    {b.items.map((item, idx) => (
+                      <li key={idx}>{item}</li>
+                    ))}
+                  </ul>
+                );
+              case BodyType.LINK:
+                return (
+                  <ul>
+                    <li key={idx}>
+                      <a href={b.href}>{b.text}</a>
+                    </li>
+                  </ul>
+                );
+            }
+          })}
       </Grid>
       <PrimaryButton
         className={classes.button}
