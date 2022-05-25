4aa9f48d59297f155134977aec07b225d3061746
mhyneman8 <mallory.hyneman@gmail.com>
2022-05-25 10:48:55 -0500

    update mod 1 content

--- src/Pages/Onboarding/OnboardingMod1.jsx
+++ src/Pages/Onboarding/OnboardingMod1.jsx
@@ -5,32 +5,30 @@ import Footer from '../../Components/Onboarding/Footer';
 
 const headerText =  {
     title: "Module 1",
-    description: "The tools we use to work together",
-    information: "You should already be deluged with invitations to create accounts for a few of the tools we use (Slack, Airtable, Google Docs). Sign up for all that stuff before reading further so the links will work. If you're missing access to any of these, drop a message in our Slack's #admin channel.",
-    information2: "These are tools you actually will be using, so please bookmark/organize them in a place easily accessible.",
-    information3: "We understand some of the tools we choose to use will be new to you. Good thing learning is fun! the best thing ever! something I do when backed into a corner. We'll be patient and helpful as you develop familiarity, and if you run into challenges, each tool has an in-house expert eager to help!"
+    description: "About Clearviction",
 };
 
-const airtable = {
+const mission = {
     icon: "",
-    title: "Airtable",
-    paragraph1: "AirTable is a relational database with a friendly (and oh-so-pretty) user interface. We use it as a the primary place to organize information, tasks, and each other. If you're new to AirTable, hold on to your Cornflakes--there's a lot to cover!",
-    paragraph2: "I'd recommend downloading AirTable onto your computer soon, and getting a feel for it--you probably already have a few tasks in your inbox!",
-    paragraph3: "You will be given super-star level editing ability, so look around and get comfortable, but, you know... be careful.",
-    link: true,
-    linkIcon: "link",
-    linkTitle: "How to use Airtable for basic tasking",
-    linkLocation: "https://docs.google.com/document/d/1he13oYe6eaC_jtaDtDFe14XWLaivzU1NLDWluRO_jXQ/edit#heading=h.1fppjcn7zale",
+    title: "Our Mission",
+    paragraph1: "ClearViction aims to reduce barriers faced by formerly incarcerated individuals by streamlining the process of vacating eligible convictions in Washington state.",
 }
 
-const slack = {
+const story = {
     icon: "",
-    title: "Slack",
-    paragraph1: "Like Churchill said of democracy, it's the worst form of communication, except for all the others. We do well to create some communal standards and come to terms with where to limit our expectations of Slack. You will receive notifications and be tagged through our channel.",
-    paragraph2: "To learn how to use Slack to communicate with the team, check the document below.",
+    title: "Our Story",
     link: true,
-    linkIcon: "link",
-    linkTitle: "Our practices",
+    linkIcon: "doc",
+    linkTitle: "Clearviction Story",
+    linkLocation: "",
+}
+
+const problem = {
+    icon: "",
+    title: "The Problem",
+    link: true,
+    linkIcon: "doc",
+    linkTitle: "The Problem",
     linkLocation: "",
 }
 
@@ -56,8 +54,9 @@ function OnboardingMod1() {
             <Header text={headerText} />
             <div>
                 <div >
-                <ModuleContent content={airtable} />
-                <ModuleContent content={slack} />
+                <ModuleContent content={mission} />
+                <ModuleContent content={story} />
+                <ModuleContent content={problem} />
                 
                 <Footer button1={button1} button2={button2} />
                 </div>
