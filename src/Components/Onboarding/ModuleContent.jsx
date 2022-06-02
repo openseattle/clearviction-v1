f0318584c725660cb5406fd7b759212b10df9eec
mhyneman8 <mallory.hyneman@gmail.com>
2022-06-02 13:11:00 -0500

    add slack modal

--- src/Components/Onboarding/ModuleContent.jsx
+++ src/Components/Onboarding/ModuleContent.jsx
@@ -7,6 +7,7 @@ import doc from '../../Assets/Onboarding/doc.svg';
 import video from '../../Assets/Onboarding/video.svg';
 
 import AirtableModal from './AirtableModal';
+import SlackModal from './SlackModal';
 
 const modalStyle = {
     position: 'absolute',
@@ -39,6 +40,10 @@ function ModuleContent(props) {
     const [airtableOpen, setAirtableOpen] = useState(false);
     const handleAirtableOpen = () => setAirtableOpen(true);
     const handleAirtableClose = () => setAirtableOpen(false);
+
+    const [slackOpen, setSlackOpen] = useState(false);
+    const handleSlackOpen = () => setSlackOpen(true);
+    const handleSlackClose = () => setSlackOpen(false);
 
     return (
         <div className={classes.moduleContentCard + " " + classes.regularContainer}>
@@ -113,6 +118,14 @@ function ModuleContent(props) {
                             }
                             { linkIcon2 === "docAirtable" && 
                                 <Button onClick={handleAirtableOpen} style={{ textDecoration: "none", color: "#4e6c99"}} >
+                                    <div className={classes.linkContainer}>
+                                        <img src={doc} alt="" className={classes.linkIcon} />
+                                        <p className={classes.linkText2} >{linkTitle2}</p>
+                                    </div>
+                                </Button>
+                            }
+                            { linkIcon2 === "docSlack" && 
+                                <Button onClick={handleSlackOpen} style={{ textDecoration: "none", color: "#4e6c99"}} >
                                     <div className={classes.linkContainer}>
                                         <img src={doc} alt="" className={classes.linkIcon} />
                                         <p className={classes.linkText2} >{linkTitle2}</p>
@@ -211,9 +224,23 @@ function ModuleContent(props) {
                     </Button>
                 </Box>
             </Modal>
-            {/* <Modal>
-                <SlackModal />
-            </Modal> */}
+
+            {/* complex slack modal */}
+            <Modal
+                open={slackOpen}
+                onClose={handleSlackClose}
+                aria-labelledby=''
+                aria-describedby=''
+                style={{maxHeight: '100vh', overflowY: 'auto', position: 'absolute', top: 0 }}
+            >
+                <Box sx={modalStyle}>
+                    <SlackModal />
+                    <Button onClick={handleSlackClose} className={classes.button}>
+                        Close
+                    </Button>
+                </Box>
+            </Modal>
+        
         </div>   
     );      
 };
