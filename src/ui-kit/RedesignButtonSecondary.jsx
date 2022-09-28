3167b6332e603b62b14f379e07caff6ebeab3c76
Robert Shepley <84053550+ShepleySound@users.noreply.github.com>
2022-09-27 18:05:07 -0700

    NEMO-1824/Recruitment cta homepage (#257)
    * Remove hanging semicolon from homepage JSX.
    
    * Alter style of RedesignButtonSecondary, currently unused in the project.
    
    * Change cta copy.
    
    * Style recruitment cta with image.
    
    * Connect recruitment link to cta button.
    
    * Run prettier on edited files.
    
    * Remove comment from component file.

--- src/ui-kit/RedesignButtonSecondary.jsx
+++ src/ui-kit/RedesignButtonSecondary.jsx
@@ -7,9 +7,11 @@ const useStyles = makeStyles(theme => ({
         height: "48px",
         borderRadius: "50px",
         padding: "16px",
+        backgroundColor: theme.palette.primary.light,
+        color: theme.palette.secondary.contrastText,
         "&:hover": {
-            backgroundColor: theme.palette.primary.dark,
-            color: theme.palette.primary.contrastText,
+            backgroundColor: theme.palette.secondary,
+            color: theme.palette.secondary.contrastText,
         },
     },
 }));
@@ -20,8 +22,9 @@ export const RedesignButtonSecondary = props => {
     return (
         <Button
             className={`${classes.RedesignButtonSecondaryStyle} ${props.classes}`}
-            variant="outlined"
-            color="primary"
+            role="button"
+            variant="contained"
+            {...props}
         >
             {props.children}
         </Button>
