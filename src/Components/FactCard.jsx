4c18d498f593922ea0aa0bb8504343516ea6dee8
kalikokalikova <calico.goodrich@gmail.com>
2022-10-06 11:02:08 -0700

    remove unneeded import from component file

--- src/Components/FactCard.jsx
+++ src/Components/FactCard.jsx
@@ -1,4 +1,4 @@
-import { Box, Card, CardContent, Typography } from "@mui/material";
+import { Card, CardContent, Typography } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 
 const useStyles = makeStyles(theme => ({
