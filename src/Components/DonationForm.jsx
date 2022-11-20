1884918e86f9b55599cf4ab25c1d97cb5733469d
Robert Shepley <84053550+ShepleySound@users.noreply.github.com>
2022-11-20 14:18:08 -0800

    Implement Donorbox embedded widget as component. (#295)

---
+++ src/Components/DonationForm.jsx
@@ -0,0 +1,25 @@
+import { Box } from "@mui/material";
+
+const DonationForm = () => {
+    return (
+        <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+            <iframe
+                title="Clearviction Donation Form"
+                src="https://donorbox.org/embed/clearviction-givingtuesday"
+                name="donorbox"
+                allowpaymentrequest="allowpaymentrequest"
+                seamless="seamless"
+                frameBorder="0"
+                height="900"
+                width="100%"
+                style={{
+                    maxWidth: "425px",
+                    minWidth: "250px",
+                    maxHeight: "none!important",
+                }}
+            />
+        </Box>
+    );
+};
+
+export default DonationForm;
