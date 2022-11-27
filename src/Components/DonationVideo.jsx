2212ef79d37ae45f4e1a5a95c6d7a394d18a7f6c
mhyneman8 <59981297+mhyneman8@users.noreply.github.com>
2022-11-27 10:25:04 -0600

    add responsiveness to video

--- src/Components/DonationVideo.jsx
+++ src/Components/DonationVideo.jsx
@@ -3,7 +3,7 @@ import ReactPlayer from "react-player";
 const DonationVideo = () => {
     return (
         <div>
-            <ReactPlayer url="https://player.vimeo.com/video/775500978" />
+            <ReactPlayer url="https://player.vimeo.com/video/775500978" width="100%" height="auto" />
         </div>
     );
 };
