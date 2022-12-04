750b8682184eac8affe51dcb95eae0995291d122
mhyneman8 <59981297+mhyneman8@users.noreply.github.com>
2022-12-04 10:55:15 -0600

    Remove reference to GT on homepage

--- src/data/siteMap.ts
+++ src/data/siteMap.ts
@@ -18,7 +18,6 @@ interface Page {
 
 const headerPages: Page[] = [
     { name: "Home", url: "/", key: PageId.Home },
-    { name: "Giving Tuesday", url: "/givingtuesday", key: PageId.GivingTuesday },
     { name: "Get Started", url: "/get-started", key: PageId.GetStarted },
     { name: "Get Involved", url: "/get-involved", key: PageId.GetInvolved },
     { name: "Why Vacate", url: "/why-vacate", key: PageId.WhyVacate },
