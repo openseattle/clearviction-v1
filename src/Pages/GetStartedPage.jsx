299839f3bfb687db43d97b7079de28dff704a556
Erik Luebbers <erluebbers@gmail.com>
2022-12-20 11:44:23 -0800

    rearrange components according to figma redesign and copy changes

--- src/Pages/GetStartedPage.jsx
+++ src/Pages/GetStartedPage.jsx
@@ -1,24 +1,25 @@
-import { Box, Container, Divider, Grid, Link, Paper, Typography } from "@mui/material";
+import { Box, Container, Divider, Grid, Typography } from "@mui/material"; // removed Link and Paper
 import { AccountBalance, DateRange, History } from "@mui/icons-material";
 
 import chart from "../Assets/chart.svg";
 import StepsIconGroup from "../Components/StepsIconGroup";
-import humanProfile from "../Assets/human.svg";
+import quickstart from "../Assets/quickstart.svg";
 import FactIconGroup from "../Components/FactIconGroup";
 import RedesignHeroPanel from "../Components/RedesignHeroPanel";
 import ResponsiveJumpButtonGroup from "../Components/ResponsiveJumpButtonGroup";
 import useGetInvolvedStyles from "../Styles/useGetInvolvedStyles";
 import { RedesignButtonPrimary } from "../ui-kit/RedesignButtonPrimary";
 import FactCard from "../Components/FactCard";
-import CourtFormsFactGroup from "../Subpages/GetStarted/CourtFormsFactGroup";
+import EligibilityFormsFactGroup from "../Subpages/GetStarted/EligibilityFormsFactGroup";
 import LegalAidServices from "../Subpages/GetStarted/LegalAidServices";
 import FinancialAidServices from "../Subpages/GetStarted/FinancialAidServices";
 import { ExternalLink } from "../ui-kit/ExternalLink";
 import { useDocumentTitle } from "../Components/customHooks/useDocumentTitle";
+import RecordInformation from "../Subpages/GetStarted/RecordInformation";
 
 const factsProps = {
     style: { fontSize: 54 },
-    color: "secondary",
+    color: "primary",
 };
 
 const eligibilityFacts = [
@@ -55,9 +56,9 @@ const GetStartedPage = () => {
                     <Grid item xs={12} sm={12} md={6}>
                         <Box component="img" style={{ width: "100%", padding: 16 }} src={chart} alt="" />
                     </Grid>
-                </Grid>
-                <Grid>
-                    <StepsIconGroup />
+                    <Grid container className={classes.steps}>
+                        <StepsIconGroup />
+                    </Grid>
                 </Grid>
             </RedesignHeroPanel>
             <Container
@@ -79,22 +80,27 @@ const GetStartedPage = () => {
                     Step 1: Documents
                 </Typography>
                 <Typography className={classes.volunteerTextStyle} variant="body1">
-                    Gathering documents is the first step in the process. This includes any forms or records that
-                    pertain to your conviction. Please gather all relevant court forms and get a copy of your Criminal
-                    History Record Information (CHRI).{" "}
+                    First let’s gather the documents you will need to determine your eligibility. Before using the
+                    calculator, gather all the documentation you have regarding your conviction.{" "}
                 </Typography>
-                <Typography className={classes.headingStyle} variant="h3">
-                    Court Forms
+                <Typography className={classes.volunteerTextStyle}>
+                    <b>You’ll need to know:</b>
                 </Typography>
-                <CourtFormsFactGroup />
+                <FactIconGroup facts={eligibilityFacts} />
                 <Typography className={classes.headingStyle} variant="h3">
-                    Criminal History Record Information (CHRI)
+                    CHRI (Recommended)
                 </Typography>
                 <Typography className={classes.volunteerTextStyle} variant="body1">
-                    You might need a copy of your Criminal History Record Information (CHRI) to vacate your record.
-                    There are two options:
+                    A copy of your <b>Criminal History Record Information (CHRI)</b> would be very helpful in the
+                    vacation process and might be required in some cases.
                 </Typography>
-                <Paper className={classes.paperStyle}>
+                <Typography className={classes.volunteerTextStyle} variant="body1" fontWeight="bold">
+                    To get a copy of your CHRI, you have two options:
+                </Typography>
+                <Grid>
+                    <RecordInformation />
+                </Grid>
+                {/* <Paper className={classes.paperStyle}>
                     <Grid container>
                         <Grid item xs={12} sm={6}>
                             <img src={humanProfile} alt="" width="100%" />
@@ -141,10 +147,11 @@ const GetStartedPage = () => {
                             </Typography>
                         </Grid>
                     </Grid>
-                </Paper>
+                </Paper> */}
             </Container>
-            <Divider className={classes.dividerStyle} />
-
+            <Container maxWidth="md">
+                <Divider className={classes.dividerStyle} />
+            </Container>
             <Container id="eligibility" component="section" className={classes.regularContainerStyle} maxWidth="md">
                 <Typography className={classes.headingStyle} variant="h2">
                     Step 2: Eligibility
@@ -153,13 +160,12 @@ const GetStartedPage = () => {
                     Once you have your records and forms gathered, use our eligibilty calculator to determine whether
                     you are eligible to vacate your misdemeanor conviction. It is expected to take 10-30 minutes.
                 </Typography>
-                <Typography className={classes.volunteerTextStyle}>
-                    <b>You’ll need to know to answer our questions:</b>
-                </Typography>
-                <FactIconGroup facts={eligibilityFacts} />
+                <Box className={classes.centerImageBox}>
+                    <Box className={classes.centeredImage} component="img" alt="" src={quickstart} />
+                </Box>
             </Container>
             <Box justifyContent="center" display="flex">
-                <RedesignButtonPrimary href="/calculator/landing-0">Check Eligibility</RedesignButtonPrimary>
+                <RedesignButtonPrimary href="/calculator/landing-0">Access Calculator</RedesignButtonPrimary>
             </Box>
             <Container maxWidth="md">
                 <Divider className={classes.dividerStyle} />
@@ -172,11 +178,13 @@ const GetStartedPage = () => {
                     Next, submit a request to have your conviction vacated (refer to{" "}
                     <ExternalLink href="https://www.courts.wa.gov/court_dir/?fa=court_dir.county">
                         court directory
-                    </ExternalLink>{" "}
+                    </ExternalLink>
                     ). Please note that the request to vacate is up to the discretion of the judge and may be denied for
                     a variety of reasons.
                 </Typography>
-                <Typography className={classes.volunteerTextStyle}>Your request to vacate may be denied if:</Typography>
+                <Typography className={classes.volunteerTextStyle}>
+                    <b>Common reasons requests to vacate may be denied:</b>
+                </Typography>
                 <Grid container>
                     <Grid item xs={12} sm={12} md={4}>
                         <FactCard
@@ -228,6 +236,10 @@ const GetStartedPage = () => {
                     Financial aid
                 </Typography>
                 <FinancialAidServices />
+                <Typography className={classes.headingStyle} variant="h3">
+                    Eligibility-Related Forms
+                </Typography>
+                <EligibilityFormsFactGroup />
             </Container>
         </>
     );
