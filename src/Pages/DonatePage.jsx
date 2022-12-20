523285cab5c4c4a51ae955629c33f56ccdef9d6b
Ubuntu <ubuntu@ip-172-31-37-199.us-east-2.compute.internal>
2022-12-20 00:57:12 +0000

    remove donation FAQ variants. Update title

--- src/Pages/DonatePage.jsx
+++ src/Pages/DonatePage.jsx
@@ -15,34 +15,30 @@ import { useGetInvolvedStyles } from "../Styles/useGetInvolvedStyles";
 const donationLevels = [
     {
         name: "level 1",
-        amount: "$10",
+        amount: "",
         donationImage: startup,
-        desc: "pay for a monthly fee for one of our technological tools",
-        tueDesc: "Fund the technological and project management tools needed to grow our project.",
+        desc: <>Fund the <Box component="span" fontWeight='bold'>technological and project management tools</Box> needed to grow our project.</>,
         tueImage: startup,
     },
     {
         name: "level 2",
-        amount: "$15",
+        amount: "",
         donationImage: discovery,
-        desc: "fund a research participant’s time ",
-        tueDesc: "Provide vouchers for users who participate in our research.",
+        desc: <>Provide <Box component="span" fontWeight='bold'>vouchers for users who participate</Box> in our research.</>,
         tueImage: discovery,
     },
     {
         name: "level 3",
-        amount: "$30",
+        amount: "",
         donationImage: laptop,
-        desc: "pay the filing fee for a low-income individual ",
-        tueDesc: "Support low-income individuals overcome financial barriers blocking the vacation of their record.",
+        desc: <>Support low-income individuals <Box component="span" fontWeight='bold'>in overcoming financial barriers</Box> blocking the vacation of their record.</>,
         tueImage: chart,
     },
     {
         name: "level 4",
-        amount: "Any $",
+        amount: "",
         donationImage: note,
-        desc: "help support those with convictions start fresh",
-        tueDesc: "Help people with convictions learn about and access our Vacation Eligibiliity calculator.",
+        desc: <>Help people with convictions <Box component="span" fontWeight='bold'>learn about and access</Box> our Vacation Eligibiliity calculator."</>,
         tueImage: searchEngineer,
     },
 ];
@@ -51,7 +47,7 @@ const DonatePage = ({ title, md }) => {
     const classes = useGetInvolvedStyles();
     return (
         <>
-            <Container component="section" id="donate" className={classes.regularContainerStyle} maxWidth="md">
+            <Container component="section" id="donate" className={classes.regularContainerStyle}>
                 <Typography className={classes.headingStyle} variant="h2">
                     {title}
                 </Typography>
@@ -63,18 +59,12 @@ const DonatePage = ({ title, md }) => {
                     {donationLevels.map(level => (
                         <Grid item key={level.name} xs={12} sm={6} md={md} lg={md}>
                             <Box display="flex" justifyContent="center">
-                                {window.location.pathname === "/givingtuesday" && (
-                                    <DonateLevelCard text={level.tueDesc} image={level.tueImage} alt="" />
-                                )}
-
-                                {window.location.pathname !== "/givingtuesday" && (
                                     <DonateLevelCard
                                         amount={level.amount}
                                         text={level.desc}
                                         image={level.donationImage}
                                         alt=""
                                     />
-                                )}
                             </Box>
                         </Grid>
                     ))}
@@ -87,9 +77,9 @@ const DonatePage = ({ title, md }) => {
                 </RedesignButtonPrimary>
             </Container>
 
-            <Container className={classes.regularContainerStyle} maxWidth="md">
+            <Container className={classes.regularContainerStyle} >
                 <Typography className={classes.headingStyle} variant="h3">
-                    Frequently Asked Questions
+                    Donation FAQ
                 </Typography>
                 <DonateFAQAccordion />
             </Container>
